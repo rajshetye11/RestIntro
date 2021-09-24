db97a03b1e1a78f31dab74b689aeba9f2a01f9dc
rajshetye11 <rajshetye11@gmail.com>
2021-09-25 00:48:34 +0530

    [raj] : added initial setup

---
+++ src/test/java/com/example/restintro/RestIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
