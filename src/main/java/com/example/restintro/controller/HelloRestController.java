2463fd7edb2639d457170e7a806b7b1c3405eebb
rajshetye11 <rajshetye11@gmail.com>
2021-09-26 23:57:52 +0530

    [raj] : added ability to display hello + name as query paramter

--- src/main/java/com/example/restintro/controller/HelloRestController.java
+++ src/main/java/com/example/restintro/controller/HelloRestController.java
@@ -1,14 +1,22 @@
 package com.example.restintro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/helloController")
 public class HelloRestController {
-	@GetMapping("/home")
-	public String home() {
-		System.out.println("Hello ");
-		return "Welcome to Rest Application";
-
-}
+	
+	@RequestMapping(value= {"","/","/home"})
+	public String sayHello() {
+		return "Hello World ";
+	}
+	
+	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
+	public String sayHello(@RequestParam(value="name")String name) {
+		return "Hello " +name+ "!";
+	}
 }
